8e5b190a921434a658542ca505cadb62a3389834
nowfeel <nowfeel@bliink.io>
2023-01-06 15:48:07 +0100

    all the needed infos for a book are correctly getted, next step: formatted correcltly all the infos and push it to a csv file

--- main.py
+++ main.py
@@ -21,25 +21,63 @@ for i in range(0, len(links), 1):
     print(i)
     res = requests.get(links[i])
     if res.ok:
+        upcValue = ""
+        priceWithTax = ""
+        priceWithoutTax = ""
+        availableValue = ""
+        imgUrl = ""
         bookPage = BeautifulSoup(res.text, "html.parser")
 
         # Get the title of the book
-        bookTitleDiv = bookPage.find("div", class_="col-sm-6 product_main")
-        titleTag = bookTitleDiv.find("h1").string
+        titleBook = bookPage.find("div", class_="col-sm-6 product_main").find("h1").string
 
-        # get the upc
-        # get the price including tax
-        # get the price exluding tax
-        # get the number available
+        # Get the UPC, price (excl/incl tax), availability information from the html page
+        productInfoTable = bookPage.find("table")
+        allTr = productInfoTable.findAll("tr")
+        for tr in allTr:
+            titleTr = tr.find("th").string
+            if titleTr == "UPC":
+                upcValue = tr.find("td").string
+            elif titleTr == "Price (excl. tax)":
+                priceWithTax = tr.find("td").string
+            elif titleTr == "Price (incl. tax)":
+                priceWithoutTax = tr.find("td").string
+            elif titleTr == "Availability":
+                availabilitySentence = tr.find("td").string.split("(")
+                sentenceSplit = availabilitySentence[1].split()
+                availableValue = sentenceSplit[0]
+
         # get the product description
-        # get the category
-        # get the review rating
-        # get the image_url
+        pTags = bookPage.findAll("p")
+        description = pTags[3].string
 
-        # initiate the dictionary
+        # get the category
+        breadCrumb = bookPage.find("ul", class_="breadcrumb")
+        liBreadCrumb = breadCrumb.findAll("li")
+        category = liBreadCrumb[2].text
+        print(category)
+
+        # get the review rating
+        pTagStars = bookPage.find("p", class_="star-rating")
+        classPtagStars = pTagStars["class"]
+        reviewRating = classPtagStars[1]
+
+        # get the image url
+        imgTag = bookPage.find("div", class_="item active").find("img")
+        imgUrl = imgTag["src"].replace("../../", "https://books.toscrape.com/")
+
+        # initiate the dictionary # at the end it will be replace by csv export
         book = dict(
             url=links[i],
-            title=titleTag,
+            title=titleBook,
+            upc=upcValue,
+            price_including_tax=priceWithTax,
+            price_excluding_tax=priceWithoutTax,
+            number_available=availableValue,
+            image_url=imgUrl,
+            product_description=description,
+            category=category,
+            review_rating=reviewRating,
         )
         books.append(book)
     else:
